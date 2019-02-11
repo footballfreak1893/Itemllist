e9cb3fb1666182d0a0809b973d82634d04aff917
Footballfreak1893 <schwabenfreak@googlemail.com>
2019-02-11 20:00:18 +0100

    outsourced Display methods in class Display

---
+++ ItemList/Classes/Display.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ItemList.Classes
+{
+    class Display
+    {
+        Start start = new Start();
+
+        public void AllItems(Data data)
+        {
+           Start.ListContainsEntries(data);
+
+            foreach (KeyValuePair<int, Item> entries in data.dict)
+            {
+                if (entries.Value.isfinished == true)
+                {
+                    Console.WriteLine(entries.Value.id + ".) " + entries.Value.title + " ->FINISHED<-");
+                }
+
+                else
+                {
+                    Console.WriteLine(entries.Value.id + ".) " + entries.Value.title);
+                }
+            }
+        }
+
+        public void ShowDetails(Data data)
+        {
+            AllItems(data);
+            Console.WriteLine("Enter ID to display details [number]");
+            string inputid;
+            inputid = Console.ReadLine();
+            if (inputid == "")
+            {
+                Console.Clear();
+                return;
+            }
+            int id = CheckingNumbers.CheckingValuesINT(inputid);
+
+
+            //int id = Convert.ToInt16(inputid);
+
+            while (!data.dict.ContainsKey(id))
+            {
+                Console.WriteLine("ID does not exist, try again");
+                AllItems(data);
+
+                Console.WriteLine("Enter ID to display details [number]");
+                inputid = Console.ReadLine();
+                id = CheckingNumbers.CheckingValuesINT(inputid);
+                Console.Clear();
+            }
+
+            Item item = data.dict[id];
+            Console.Clear();
+            Console.WriteLine("ID: " + item.id);
+            Console.WriteLine("Title: " + item.title);
+            Console.WriteLine("Description: " + item.description);
+            Console.WriteLine("Createdate: " + item.createdate.ToShortDateString());
+            if (item.enddate.Equals("0001, 01, 01")) //--> Checking Date Value
+            {
+                Console.WriteLine("Enddate: ");
+            }
+
+            else
+            {
+                Console.WriteLine("Enddate: " + item.enddate.ToShortDateString());
+            }
+            Console.WriteLine();
+            Console.WriteLine("Update Item [u]");
+            Console.WriteLine("Delete Item [d]");
+            Console.WriteLine("Set entry finish [f]");
+            Console.WriteLine("Exit Programm [e]");
+            string inputvalue = Console.ReadLine();
+
+            switch (inputvalue)
+            {
+                case "u":
+                    Console.Clear();
+                    data.UpdateItem(id, item);
+                    Console.Clear();
+                    break;
+
+                case "d":
+                    Console.Clear();
+                    data.DeleteItem(id, item);
+                    break;
+
+                case "f":
+                    Console.Clear();
+                    item.isfinished = true;
+                    Console.WriteLine("Set is finsihed"); //--> Evtl eigene Methode
+                    data.SaveList(data.path);
+                    break;
+
+                case "e":
+                    Console.Clear();
+                    Start.Exit(data);
+                    break;
+
+                default:
+                    Console.Clear();
+                    return;
+            }
+        }
+    }
+}
--- ItemList/Classes/Start.cs
+++ ItemList/Classes/Start.cs
@@ -7,11 +7,13 @@ namespace ItemList
 {
     public class Start
     {
+         
 
         static void Main(string[] args)
         {
             Data data = new Data();
             StartProgramm(data);
+            Display display = new Display();
         }
 
         public static void StartProgramm(Data data)
@@ -19,6 +21,7 @@ namespace ItemList
             string path = "data";
             data.FileExists();
             string version = "v 1.0";
+            Display display = new Display();
 
             while (true)
             {
@@ -43,7 +46,7 @@ namespace ItemList
 
                     case "s":
                         Console.Clear();
-                        DisplayAllItems(data);
+                        display.AllItems(data);
                         break;
 
                     case "e":
@@ -53,7 +56,7 @@ namespace ItemList
 
                     case "x":
                         Console.Clear();
-                        ShowDetails(data);
+                        display.ShowDetails(data);
                         break;
 
                     case "r":
@@ -64,7 +67,7 @@ namespace ItemList
                         break;
                     default:
                         Console.Clear();
-                        DisplayAllItems(data);
+                        display.AllItems(data);
                         Console.Clear();
                         break;
                 }
@@ -78,104 +81,6 @@ namespace ItemList
             data.SaveId(data.pathId, data.currentid);
             Environment.Exit(1);
 
-        }
-
-        public static void ShowDetails(Data data)
-        {
-            DisplayAllItems(data);
-            Console.WriteLine("Enter ID to display details [number]");
-            string inputid;
-            inputid = Console.ReadLine();
-            if (inputid == "")
-            {
-                Console.Clear();
-                return;
-            }
-            int id =   CheckingNumbers.CheckingValuesINT(inputid);
-            
-            
-            //int id = Convert.ToInt16(inputid);
-
-            while (!data.dict.ContainsKey(id))
-            {
-                Console.WriteLine("ID does not exist, try again");
-                DisplayAllItems(data);
-
-                Console.WriteLine("Enter ID to display details [number]");
-                inputid = Console.ReadLine();
-                id = CheckingNumbers.CheckingValuesINT(inputid);
-                Console.Clear();
-            }
-
-            Item item = data.dict[id];
-            Console.Clear();
-            Console.WriteLine("ID: " + item.id);
-            Console.WriteLine("Title: " + item.title);
-            Console.WriteLine("Description: " + item.description);
-            Console.WriteLine("Createdate: " + item.createdate.ToShortDateString());
-            if (item.enddate.Equals("0001, 01, 01")) //--> Checking Date Value
-            {
-                Console.WriteLine("Enddate: ");
-            }
-
-            else
-            {
-                Console.WriteLine("Enddate: " + item.enddate.ToShortDateString());
-            }
-            Console.WriteLine();
-            Console.WriteLine("Update Item [u]");
-            Console.WriteLine("Delete Item [d]");
-            Console.WriteLine("Set entry finish [f]");
-            Console.WriteLine("Exit Programm [e]");
-            string inputvalue = Console.ReadLine();
-
-            switch (inputvalue)
-            {
-                case "u":
-                    Console.Clear();
-                    data.UpdateItem(id, item);
-                    Console.Clear();
-                    break;
-
-                case "d":
-                    Console.Clear();
-                    data.DeleteItem(id, item);
-                    break;
-
-                case "f":
-                    Console.Clear();
-                    item.isfinished = true;
-                    Console.WriteLine("Set is finsihed"); //--> Evtl eigene Methode
-                    data.SaveList(data.path);
-                    break;
-
-                case "e":
-                    Console.Clear();
-                    Exit(data);
-                    break;
-
-                default:
-                    Console.Clear();
-                    return;
-            }
-        }
-
-        public static void DisplayAllItems(Data data)
-        {
-            ListContainsEntries(data);
-            
-            foreach (KeyValuePair<int, Item> entries in data.dict)
-            {
-                if (entries.Value.isfinished == true)
-                {
-                    Console.WriteLine(entries.Value.id + ".) " +entries.Value.title +" ->FINISHED<-");
-                }
-               
-                else
-                {
-                    Console.WriteLine(entries.Value.id + ".) " +entries.Value.title);
-                }
-            }
         }
 
         public static void ListContainsEntries(Data data)
