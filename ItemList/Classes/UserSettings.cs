0c708cbf9918224dadf8f4e3d6b80969e81cd3d2
Christian Wachter <christian_wachter@orbit.de>
2019-05-02 15:05:39 +0200

    added class UserSettings

---
+++ ItemList/Classes/UserSettings.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ItemList.Classes
+{
+    class UserSettings
+    {
+        public void AddToAutoStart()
+        {
+            string pathExe = @"C:\_P\Privat\List\Itemllist\ItemList\bin\Debug";
+
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.Startup);
+        }
+
+        public void SelectAttributes()
+        {
+            
+        }
+    
+    }
+}
